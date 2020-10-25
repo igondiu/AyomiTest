825b373190735ef3fb821340725ddd035075bee3
igondiu <ion.gondiu@ibm.com>
2020-10-25 22:59:41 +0100

    clean up

---
+++ setup/setup.py
@@ -0,0 +1,13 @@
+from libs.setup_logger import setup_logger
+
+
+def logger_setup(cfg):
+    """ Instantiates the logger by calling the function setup_logger() in the lib setup_logger.py"""
+
+    micro_service_name = "ayomi_test"
+    logger = setup_logger(cfg.LOG_DIR, micro_service_name, debug_verbosity=cfg.LOG_VERBOSITY)
+    if logger[0]:
+        log = logger[1]
+    elif logger[0] is False:
+        print(("Unable to instantiate logs due to exception: {}. Exiting.".format(logger[1])))
+        exit(1)
